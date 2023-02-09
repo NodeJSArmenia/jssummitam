16826ef81c836fba9135af6c2667c03b2f3e2300
Vahagn Melkonyan <vahmelk99@gmail.com>
2023-02-09 22:22:38 +0400

    added schedule part without data

--- src/Header/Header.vue
+++ src/Header/Header.vue
@@ -16,6 +16,7 @@
 				<!-- <a class="Item" href="#Sponsors" v-smooth-scroll="navConfig" v-on:click="close">Sponsor</a> -->
 				<a class="Item" href="#Ninjas" v-smooth-scroll="navConfig" v-on:click="close">Ninjas</a>
 				<a class="Item" href="#Team" v-smooth-scroll="navConfig" v-on:click="close">Team</a>
+				<a class="Item" href="#Schedule" v-smooth-scroll="navConfig" v-on:click="close">Schedule</a>
 				<a class="Item" href="#Community" v-smooth-scroll="navConfig" v-on:click="close">Community</a>
 				<a class="Item" href="#Location" v-smooth-scroll="navConfig" v-on:click="close">Location</a>
 				<!-- <a class="Item" href="#Faq" v-smooth-scroll="navConfig" v-on:click="close">Faq</a> -->
@@ -28,200 +29,200 @@
 </template>
 
 <script>
-import _throttle from 'lodash/throttle';
-import Logo from '../_common/Logo/Logo.vue';
-import Button from '../_common/Button/Button.vue';
+import _throttle from "lodash/throttle";
+import Logo from "../_common/Logo/Logo.vue";
+import Button from "../_common/Button/Button.vue";
 
 export default {
-	components: {
-		Logo,
-		Button,
-	},
-	data() {
-		return {
-			scrollPos: window.scrollY,
-			isOpen: false,
-			navConfig: {
-				duration: 2000,
-				offset: -66,
-			},
-		};
-	},
-	methods: {
-		handleScroll() {
-			this.scrollPos = window.scrollY;
-		},
-		toggle(event) {
-			event.preventDefault();
+    components: {
+        Logo,
+        Button
+    },
+    data() {
+        return {
+            scrollPos: window.scrollY,
+            isOpen: false,
+            navConfig: {
+                duration: 2000,
+                offset: -66
+            }
+        };
+    },
+    methods: {
+        handleScroll() {
+            this.scrollPos = window.scrollY;
+        },
+        toggle(event) {
+            event.preventDefault();
 
-			this.isOpen = !this.isOpen;
-		},
-		close() {
-			this.isOpen = false;
-		},
-	},
-	computed: {
-		isSticked() {
-			return this.scrollPos > window.innerHeight * 0.8;
-		},
-	},
-	created() {
-		window.addEventListener('scroll', _throttle(this.handleScroll, 100));
-	},
-	destroyed() {
-		window.removeEventListener('scroll', this.handleScroll);
-	},
+            this.isOpen = !this.isOpen;
+        },
+        close() {
+            this.isOpen = false;
+        }
+    },
+    computed: {
+        isSticked() {
+            return this.scrollPos > window.innerHeight * 0.8;
+        }
+    },
+    created() {
+        window.addEventListener("scroll", _throttle(this.handleScroll, 100));
+    },
+    destroyed() {
+        window.removeEventListener("scroll", this.handleScroll);
+    }
 };
 </script>
 
 <style scoped lang="scss">
-	@import '../_styles/colors';
+@import "../_styles/colors";
 
-	.Header {
-		display: flex;
-		min-height: 100px;
-		align-items: center;
-		padding: .5em 0;
-		position: fixed;
-		width: 100%;
-		top: -150px;
-		left: 0;
-		z-index: 100;
-		background: $bg-primary;
-		transition: top 0.3s cubic-bezier(0.02, 0.01, 1, 0.6);
-		border-bottom: 1px solid $green-primary;;
+.Header {
+    display: flex;
+    min-height: 100px;
+    align-items: center;
+    padding: 0.5em 0;
+    position: fixed;
+    width: 100%;
+    top: -150px;
+    left: 0;
+    z-index: 100;
+    background: $bg-primary;
+    transition: top 0.3s cubic-bezier(0.02, 0.01, 1, 0.6);
+    border-bottom: 1px solid $green-primary;
 
-		&.-sticked {
-			display: flex;
-			position: fixed;
-			top: 0;
-			min-height: 80px;
-			background: $bg-primary;
-			transition: top 0.3s cubic-bezier(0.02, 0.01, 1, 0.6);
-		}
+    &.-sticked {
+        display: flex;
+        position: fixed;
+        top: 0;
+        min-height: 80px;
+        background: $bg-primary;
+        transition: top 0.3s cubic-bezier(0.02, 0.01, 1, 0.6);
+    }
+}
 
-	}
+.LogoContainer,
+.NavBtn {
+    z-index: 999;
+}
 
-	.LogoContainer, .NavBtn {
-		z-index: 999;
-	}
+.HeaderContainer {
+    max-width: 1400px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    position: relative;
+    width: 100%;
+    padding-left: 130px;
+}
 
-	.HeaderContainer {
-		max-width: 1400px;
-		display: flex;
-		justify-content: flex-end;
-		align-items: center;
-		position: relative;
-		width: 100%;
-		padding-left: 130px;
-	}
+.LogoContainer {
+    position: absolute;
+    left: 15px;
+    z-index: 2;
+}
 
-	.LogoContainer {
-		position: absolute;
-		left: 15px;
-		z-index: 2;
-	}
+.Navigation {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: flex-end;
+    font-family: Barlow;
+    font-size: 16px;
+    font-weight: bold;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.25;
+    letter-spacing: 0.5px;
 
-	.Navigation {
-		display: flex;
-		flex-direction: row;
-		flex-wrap: wrap;
-		justify-content: flex-end;
-		font-family: Barlow;
-		font-size: 16px;
-		font-weight: bold;
-		font-stretch: normal;
-		font-style: normal;
-		line-height: 1.25;
-		letter-spacing: 0.5px;
+    .Item {
+        display: flex;
+        align-items: center;
+        padding: 0.5em 0.8em;
+        color: $text-white;
+        margin: 0 0.1em;
+        text-decoration: none;
+        font-weight: 600;
+        transition: all 0.2s ease;
+        position: relative;
+        overflow: hidden;
 
-		.Item {
-			display: flex;
-			align-items: center;
-			padding: .5em .8em;
-			color: $text-white;
-			margin: 0 .1em;
-			text-decoration: none;
-			font-weight: 600;
-			transition: all .2s ease;
-			position: relative;
-			overflow: hidden;
+        &:after {
+            content: " ";
+            display: block;
+            background-color: $gray-secondary;
+            position: absolute;
+            transition: all 0.25s ease;
+            top: 100%;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            z-index: -1;
+        }
 
-			&:after {
-				content: " ";
-				display: block;
-				background-color: $gray-secondary;
-				position: absolute;
-				transition: all .25s ease;
-				top: 100%;
-				left: 0;
-				width: 100%;
-				height: 100%;
-				z-index: -1;
-			}
+        &.active,
+        &:hover {
+            color: $green-primary;
+            // border-bottom: 2px solid $green-primary;
+        }
+    }
 
-			&.active,
-			&:hover {
-				color: $green-primary;
-				// border-bottom: 2px solid $green-primary;
-			}
-		}
+    .RequestInvite {
+        display: inherit;
+        margin: 10px 0px 10px 0px;
+        z-index: 0;
+    }
+}
 
-		.RequestInvite {
-			display: inherit;
-			margin: 10px 0px 10px 0px;
-			z-index: 0;
-		}
-	}
+.NavBtn {
+    display: none;
+}
 
-	.NavBtn {
-		display: none;
-	}
+@media (max-width: 650px) {
+    .Navigation {
+        position: absolute;
+        left: 0;
+        top: -500px;
+        background: $bg-primary;
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        padding: 10px;
+        align-items: center;
+        margin-top: 50px;
+        transition: top 0.6s ease;
+    }
 
-	@media (max-width: 650px) {
-		.Navigation {
-			position: absolute;
-			left: 0;
-			top: -500px;
-			background: $bg-primary;
-			display: flex;
-			flex-direction: column;
-			width: 100%;
-			padding: 10px;
-			align-items: center;
-			margin-top: 50px;
-			transition: top .6s ease;
-		}
+    .Header.-open .Navigation {
+        top: 0;
+        transition: top 0.6s ease;
+    }
 
-		.Header.-open .Navigation {
-			top: 0;
-			transition: top .6s ease;
-		}
+    .Navigation .Item {
+        justify-content: center;
+    }
 
-		.Navigation .Item {
-			justify-content: center;
-		}
+    .NavigationButton {
+        display: inline;
+    }
 
-		.NavigationButton {
-			display: inline;
-		}
+    .HeaderContainer {
+        position: initial;
+    }
 
-		.HeaderContainer {
-			position: initial;
-		}
+    .NavBtn {
+        display: inline;
+        top: 0;
+        position: absolute;
+        right: 0;
+        color: #ffde59 !important;
+        margin: 15px 25px;
+        font-size: 30px;
+    }
 
-		.NavBtn {
-			display: inline;
-			top: 0;
-			position: absolute;
-			right: 0;
-			color: #ffde59 !important;
-			margin: 15px 25px;
-			font-size: 30px;
-		}
-
-		.Header.-open {
-			background: $bg-primary;
-		}
-	}
+    .Header.-open {
+        background: $bg-primary;
+    }
+}
 </style>
