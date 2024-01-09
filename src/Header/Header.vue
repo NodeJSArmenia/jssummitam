cd31a4f3615fdf2f08d1f0faacbcca2e7cec3eaf
Nairi Harutyunyan <nairi.haroutiounian@gmail.com>
2024-01-09 09:54:45 +0400

    improve header order

--- src/Header/Header.vue
+++ src/Header/Header.vue
@@ -12,8 +12,8 @@
 			</a>
 			<nav class="Navigation" v-smooth-scroll="navConfig">
 				<a class="Item" href="#Intro" v-smooth-scroll="navConfig" v-on:click="close">Intro</a>
-				<a class="Item" href="#Speakers" v-smooth-scroll="navConfig" v-on:click="close">Speakers</a>
                 <a class="Item" href="#Sponsors" v-smooth-scroll="navConfig" v-on:click="close">Sponsors</a>
+				<a class="Item" href="#Speakers" v-smooth-scroll="navConfig" v-on:click="close">Speakers</a>
 				<!-- <a class="Item" href="#Ninjas" v-smooth-scroll="navConfig" v-on:click="close">Ninjas</a> -->
 				<a class="Item" href="#Organizers" v-smooth-scroll="navConfig" v-on:click="close">Organizers</a>
 				<!-- <a class="Item" href="#Schedule" v-smooth-scroll="navConfig" v-on:click="close">Schedule</a> -->
